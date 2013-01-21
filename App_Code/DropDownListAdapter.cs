80e04008813043ec78208e76938c48a6c6efad4d
Christoff Truter <christoff@cstruter.com>
2013-01-21 18:48:49 +0200

    First version

---
+++ App_Code/DropDownListAdapter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls.Adapters;
+using System.Web.UI.WebControls;
+
+namespace CSTruter
+{
+    public class DropDownListAdapter : WebControlAdapter
+    {
+        private Object _ViewState;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                if (_ViewState != null)
+                {
+                    Object[] groups = (Object[])_ViewState;
+                    DropDownList dropDownList = (DropDownList)Control;
+                    // Add saved optgroups to ListItems
+                    for (Int32 i = 0; i < groups.Length; i++)
+                    {
+                        if (groups[i] != null)
+                        {
+                            dropDownList.Items[i].Attributes["Group"] = groups[i].ToString();
+                        }
+                    }
+                }
+            }
+            base.OnLoad(e);
+        }
+
+        protected override void LoadAdapterViewState(object state)
+        {
+            // Retrieve existing state
+            _ViewState = state;
+        }
+
+        protected override object SaveAdapterViewState()
+        {
+            DropDownList dropDownList = (DropDownList)Control;
+            Int32 count = dropDownList.Items.Count;
+            Object[] values = new Object[count];
+
+            // Retrieve Optgrouping from ListItem 
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = dropDownList.Items[i].Attributes["Group"];
+            }
+            return values;
+        }
+
+        protected override void RenderContents(System.Web.UI.HtmlTextWriter writer)
+        {
+            // The current control being "adaptered" is available within context from the Control property
+            DropDownList dropDownList = (DropDownList)Control;
+            ListItemCollection items = dropDownList.Items;
+
+            // Retrieve Optgrouping using LinQ
+            var groups = (from p in items.OfType<ListItem>()
+                          group p by p.Attributes["Group"] into g
+                          select new { Label = g.Key, Items = g.ToList<ListItem>() });
+
+            foreach (var group in groups)
+            {
+                if (!String.IsNullOrEmpty(group.Label))
+                {
+                    writer.WriteBeginTag("optgroup");
+                    writer.WriteAttribute("label", group.Label);
+                    writer.Write(">");
+                }
+
+                int count = group.Items.Count();
+                if (count > 0)
+                {
+                    bool flag = false;
+                    for (int i = 0; i < count; i++)
+                    {
+                        ListItem item = group.Items[i];
+
+                        writer.WriteBeginTag("option");
+                        if (item.Selected)
+                        {
+                            if (flag)
+                            {
+                                throw new HttpException("Multiple selected items not allowed");
+                            }
+                            flag = true;
+
+                            writer.WriteAttribute("selected", "selected");
+                        }
+
+                        if (!item.Enabled)
+                        {
+                            writer.WriteAttribute("disabled", "true");
+                        }
+
+                        writer.WriteAttribute("value", item.Value, true);
+
+                        if (this.Page != null)
+                        {
+                            this.Page.ClientScript.RegisterForEventValidation(dropDownList.UniqueID, item.Value);
+                        }
+                        writer.Write('>');
+                        HttpUtility.HtmlEncode(item.Text, writer);
+                        writer.WriteEndTag("option");
+                        writer.WriteLine();
+                    }
+                }
+                if (!String.IsNullOrEmpty(group.Label))
+                {
+                    writer.WriteEndTag("optgroup");
+                }
+            }
+        }
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList runat="server" ID="ddlA">
+            <asp:ListItem Text="C#" Value="1" Group="Microsoft"></asp:ListItem>
+            <asp:ListItem Text="VB.net" Value="2" Group="Microsoft"></asp:ListItem>
+            <asp:ListItem Text="PHP" Value="3" Group="Open Source"></asp:ListItem>
+            <asp:ListItem Text="Java" Value="4" Group="Open Source" Enabled="false"></asp:ListItem>
+            <asp:ListItem Text="Perl" Value="5" Group="Open Source"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button runat="server" ID="btnSubmit" Text="Submit" />
+    </div>
+    </form>
+</body>
+</html>
